3f032e35b537ce9b28bd483576fb1f2d7b72a85d
Christopher Lee <hanernlee@gmail.com>
2017-11-13 09:19:11 +1100

    First commit

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Christopher Lee on 13/11/17.
+//  Copyright © 2017 Christopher Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello World", message: "This is my App", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+
+}
+
