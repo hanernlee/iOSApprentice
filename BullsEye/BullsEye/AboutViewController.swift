317b22ef11d3cfd4f0b630b5e8ce0903091d0497
Christopher Lee <hanernlee@gmail.com>
2017-11-15 00:55:20 +1100

    WebView

--- BullsEye/BullsEye/AboutViewController.swift
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class AboutViewController: UIViewController {
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+            if let htmlData = try? Data(contentsOf: url) {
+                let baseURL = URL(fileURLWithPath: Bundle.main.bundlePath)
+                webView.load(htmlData, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+            }
+        }
+        
+    }
+    
+    @IBOutlet weak var webView: UIWebView!
+    
     @IBAction func close() {
         dismiss(animated: true, completion: nil)
     }
