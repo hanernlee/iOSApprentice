dc75ff6d83c43ba0bfb1e826ca9eca7816ee2813
Christopher Lee <hanernlee@gmail.com>
2017-11-21 18:21:44 +1100

    Fetch Request with core data

---
+++ MyLocations/MyLocations/LocationsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  LocationsViewController.swift
+//  MyLocations
+//
+//  Created by Christopher Lee on 21/11/17.
+//  Copyright © 2017 Christopher Lee. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class LocationsViewController: UITableViewController {
+    
+    var managedObjectContext: NSManagedObjectContext!
+    
+    var locations = [Location]()
+    
+    // MARK:- View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fetchRequest = NSFetchRequest<Location>()
+        
+        let entity = Location.entity()
+        fetchRequest.entity = entity
+        
+        let sortDescriptor = NSSortDescriptor(key: "date", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        do {
+            locations = try managedObjectContext.fetch(fetchRequest)
+        } catch {
+            fatalCoreDataError(error)
+        }
+        
+    }
+    
+    // MARK:- Table View Delegates
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath)
+        
+        let descriptionLabel = cell.viewWithTag(100) as! UILabel
+        descriptionLabel.text = "If you can see this"
+        
+        let addressLabel = cell.viewWithTag(101) as! UILabel
+        addressLabel.text = "Then it works!"
+        
+        return cell
+    }
+
+}
