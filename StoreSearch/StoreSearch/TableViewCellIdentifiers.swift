2cb22c1b440a6f5cc65a831f7addb81dc72bba3e
Christopher Lee <hanernlee@gmail.com>
2017-11-24 01:09:23 +1100

    Refactor using singletons

---
+++ StoreSearch/StoreSearch/TableViewCellIdentifiers.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewCellIdentifiers.swift
+//  StoreSearch
+//
+//  Created by Christopher Lee on 24/11/17.
+//  Copyright © 2017 Christopher Lee. All rights reserved.
+//
+
+import Foundation
+
+struct TableViewCellIdentifiers {
+    static let searchResultCell = "SearchResultCell"
+}
