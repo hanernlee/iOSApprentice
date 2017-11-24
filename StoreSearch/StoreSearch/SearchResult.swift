f98ca936b3e978b9ff63bae74498b0cc33b80123
Christopher Lee <hanernlee@gmail.com>
2017-11-25 01:36:47 +1100

    JSON Codable

--- StoreSearch/StoreSearch/SearchResult.swift
+++ StoreSearch/StoreSearch/SearchResult.swift
@@ -8,7 +8,37 @@
 
 import Foundation
 
-class SearchResult {
-    var name = ""
-    var artistName = ""
+class ResultArray:Codable {
+    var resultCount = 0
+    var results = [SearchResult]()
 }
+
+class SearchResult:Codable, CustomStringConvertible {
+    var artistName = ""
+    var trackName = ""
+    var kind = ""
+    var trackPrice = 0.0
+    var currency = ""
+    var imageSmall = ""
+    var imageLarge = ""
+    var storeURL = ""
+    var genre = ""
+
+    enum CodingKeys: String, CodingKey {
+        case imageSmall = "artworkUrl60"
+        case imageLarge = "artworkUrl100"
+        case storeURL = "trackViewUrl"
+        case genre = "primaryGenreName"
+        case kind, artistName, trackName
+        case trackPrice, currency
+    }
+
+    var name: String {
+        return trackName
+    }
+
+    var description: String {
+        return "Kind: \(kind), Name: \(name), Artist name: \(artistName)\n"
+    }
+}
+
