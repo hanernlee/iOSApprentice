3eceb8a5ba270a16d58ebfa2254d994186545c7f
Christopher Lee <hanernlee@gmail.com>
2017-11-25 12:24:30 +1100

    Parsing JSON

--- StoreSearch/StoreSearch/SearchResult.swift
+++ StoreSearch/StoreSearch/SearchResult.swift
@@ -15,30 +15,54 @@ class ResultArray:Codable {
 
 class SearchResult:Codable, CustomStringConvertible {
     var artistName = ""
-    var trackName = ""
-    var kind = ""
-    var trackPrice = 0.0
+    var kind: String?
+    var trackName: String?
+    var trackPrice: Double?
+    var trackViewUrl: String?
+    var collectionName: String?
+    var collectionViewUrl: String?
+    var collectionPrice: Double?
+    var itemPrice: Double?
+    var itemGenre: String?
+    var bookGenre: [String]?
     var currency = ""
     var imageSmall = ""
     var imageLarge = ""
-    var storeURL = ""
-    var genre = ""
 
     enum CodingKeys: String, CodingKey {
         case imageSmall = "artworkUrl60"
         case imageLarge = "artworkUrl100"
-        case storeURL = "trackViewUrl"
-        case genre = "primaryGenreName"
-        case kind, artistName, trackName
-        case trackPrice, currency
+        case itemGenre = "primaryGenreName"
+        case bookGenre = "genre"
+        case itemPrice = "price"
+        case kind, artistName, currency
+        case trackName, trackPrice, trackViewUrl
+        case collectionName, collectionPrice, collectionViewUrl
     }
 
     var name: String {
-        return trackName
+        return trackName ?? collectionName ?? ""
     }
 
     var description: String {
-        return "Kind: \(kind), Name: \(name), Artist name: \(artistName)\n"
+        return "Kind: \(kind ?? ""), Name: \(name), Artist name: \(artistName)\n"
+    }
+    
+    var storeURL: String {
+        return trackViewUrl ?? collectionViewUrl ?? ""
+    }
+    
+    var price: Double {
+        return trackPrice ?? collectionPrice ?? itemPrice ?? 0.0
+    }
+    
+    var genre: String {
+        if let genre = itemGenre {
+            return genre
+        } else if let genres = bookGenre {
+            return genres.joined(separator: ", ")
+        }
+        return ""
     }
 }
 
