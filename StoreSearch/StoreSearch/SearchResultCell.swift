217bb7196be4512c9f51265d894cd7518ee8cbc5
Christopher Lee <hanernlee@gmail.com>
2017-11-26 14:29:57 +1100

    Cancel download task when reusing cells

--- StoreSearch/StoreSearch/SearchResultCell.swift
+++ StoreSearch/StoreSearch/SearchResultCell.swift
@@ -14,6 +14,8 @@ class SearchResultCell: UITableViewCell {
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var artworkImageView: UIImageView!
     
+    var downloadTask: URLSessionDownloadTask?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -22,10 +24,32 @@ class SearchResultCell: UITableViewCell {
         selectedBackgroundView = selectedView
     }
 
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        downloadTask?.cancel()
+        downloadTask = nil
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
 
+    // MARK:- Public Methods
+    func configure(for result: SearchResult) {
+        nameLabel.text = result.name
+        
+        if result.artistName.isEmpty {
+            artistNameLabel.text = "Unknown"
+        } else {
+            artistNameLabel.text = String(format: "%@ (%@)", result.artistName, result.type)
+        }
+        
+        artworkImageView.image = UIImage(named: "Placeholder")
+        if let smallURL = URL(string: result.imageSmall) {
+            downloadTask = artworkImageView.loadImage(url: smallURL)
+        }
+        
+    }
 }
---
+++ StoreSearch/StoreSearch/UIImageView+DownloadImage.swift
@@ -0,0 +1,33 @@
+//
+//  UIImageView+DownloadImage.swift
+//  StoreSearch
+//
+//  Created by Christopher Lee on 26/11/17.
+//  Copyright © 2017 Christopher Lee. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    func loadImage(url: URL) -> URLSessionDownloadTask {
+        let session = URLSession.shared
+        
+        let downloadTask = session.downloadTask(with: url, completionHandler: {
+            [weak self] url, response, error in
+            
+            if error == nil, let url = url,
+                let data = try? Data(contentsOf: url),
+                let image = UIImage(data: data) {
+             
+                DispatchQueue.main.async {
+                    if let weakSelf = self {
+                        weakSelf.image = image
+                    }
+                }
+            }
+        })
+        
+        downloadTask.resume()
+        return downloadTask
+    }
+}
